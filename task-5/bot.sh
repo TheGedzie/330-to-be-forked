b6a875a2836cd5ea4838aa57f2c7941dcb8d0d6b
YOUR NAME <YOUR EMAIL ADDRESS>
2024-10-15 17:37:03 +0700

    Bot

--- task-5/bot.sh
+++ task-5/bot.sh
@@ -3,4 +3,4 @@
 echo Введите текст:
 read text
 
-xdg-open https://api.telegram.org/bot7619219746:AAGaa2kxUVJXG17jYPcODg0By46I2CJIV_4/sendMessage?chat_id=1081998754&text=$text
+curl https://api.telegram.org/bot7619219746:AAGaa2kxUVJXG17jYPcODg0By46I2CJIV_4/sendMessage?chat_id=1081998754&text=$text
