be44a004e629ef0f374fce7cf1ee9365cf4bc55f
YOUR NAME <YOUR EMAIL ADDRESS>
2024-10-15 16:48:47 +0700

    Task-4

---
+++ task-4/scripts/proba.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for((i = 2020; i <= 2024;i++))
+do
+  echo $i 
+done
--- task-4/scripts/users_structures.sh
+++ task-4/scripts/users_structures.sh
@@ -2,12 +2,15 @@
 
 cd ~
 mkdir users
+cd users
 
 echo Введите имя директории:
 read dir_name
 
 mkdir $dir_name
 cd $dir_name
+
+git init 
 
 mkdir photos
 cd photos
@@ -19,3 +22,33 @@ do
   touch default.png
   cd ..  
 done
+cd .. 
+git add photos
+git commit -m "Photos"
+
+mkdir videos
+cd videos
+
+for((i = 2020; i <= 2024;i++))
+do
+  mkdir $i
+  cd $i
+  touch default.mp4
+  cd ..  
+done
+cd .. 
+git add videos
+git commit -m "Videos"
+
+mkdir documents
+cd documents
+for((i = 2020; i <= 2024;i++))
+do
+  mkdir $i
+  cd $i
+  touch default.png
+  cd ..  
+done
+cd .. 
+git add documents
+git commit -m "Documents"
